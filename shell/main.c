e9c3fc77543be4392a033b4b0d441d2555ac1f5c
Wang Renxin <hellotony521@qq.com>
2015-09-23 11:50:40 +0800

    *updated main.c.

--- shell/main.c
+++ shell/main.c
@@ -382,7 +382,7 @@ static char* _load_file(const char* path) {
 		fseek(fp, curpos, SEEK_SET);
 		result = (char*)malloc((size_t)(l + 1));
 		mb_assert(result);
-		(void)fread(result, 1, l, fp);
+		fread(result, 1, l, fp);
 		fclose(fp);
 		result[l] = '\0';
 	}
@@ -415,9 +415,9 @@ static int _save_file(const char* path, const char* txt) {
 
 static void _clear_screen(void) {
 #ifdef _MSC_VER
-	(void)system("cls");
+	system("cls");
 #else /* _MSC_VER */
-	(void)system("clear");
+	system("clear");
 #endif /* _MSC_VER */
 }
 
