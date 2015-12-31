a253352ffb3a9970614aafc661f42e55a87ac6be
paladin-t <hellotony521@qq.com>
2015-12-31 10:18:38 +0800

    +added a new sample script source file; *updated clean shell.

--- clean.cmd
+++ clean.cmd
@@ -1,21 +1,25 @@
 @echo off
 echo Cleaning, a moment please...
 
+rd /s /q .vs
+rd /s /q temp
+
 attrib *.suo -s -r -h
+
+del /f /s /q *.exp
+del /f /s /q *.log
 del /f /s /q *.ncb
+del /f /s /q *.obj
+del /f /s /q *.pdb
 del /f /s /q *.sdf
 del /f /s /q *.suo
 del /f /s /q *.user
-
-rd /s /q temp
 
 del /f /s /q output\*.dep
 del /f /s /q output\*.htm
 del /f /s /q output\*.idb
 del /f /s /q output\*.ilk
 del /f /s /q output\*.manifest
-del /f /s /q output\*.obj
-del /f /s /q output\*.pdb
 del /f /s /q output\*.res
 del /f /s /q output\my_basic_d.exe
 
---
+++ sample/sample06.bas
@@ -0,0 +1,27 @@
+﻿' This script is an example of MY-BASIC
+' Copyright (c) 2011 - 2016 Wang Renxin. All rights reserved.
+' For more information, see https://github.com/paladin-t/my_basic/
+
+class animal
+    def speak(a)
+        print "default" + a;
+    enddef
+endclass
+
+class cat(animal)
+    def speak(a)
+        print "meow" + a;
+    enddef
+endclass
+
+class dog(animal)
+    def speak(a)
+        print "bark" + a;
+    enddef
+endclass
+
+c = new(cat)
+d = new(dog)
+
+c.speak("!")
+d.speak("!")
