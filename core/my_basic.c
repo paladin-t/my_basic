134804e4f571e23258b5475bd53969f81b91ae9d
Wang Renxin <hellotony521@qq.com>
2016-06-21 10:47:21 +0800

    *polished code.

--- core/my_basic.c
+++ core/my_basic.c
@@ -12240,10 +12240,8 @@ int mb_debug_get_stack_trace(struct mb_interpreter_t* s, void** l, char** fs, un
 	mb_assert(s);
 
 	if(fs && fc) {
-		if(_ls_count(s->stack_frames) > fc) {
-			fs[fc - 1] = "...";
-			fc--;
-		}
+		if(_ls_count(s->stack_frames) > fc)
+			fs[--fc] = "...";
 		f = s->stack_frames->prev;
 		while(f != s->stack_frames && f && f->data && i < fc) {
 			fs[i++] = (char*)f->data;
