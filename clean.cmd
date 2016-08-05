150f86b5a1a6f18fe80562c57ace452032339b7a
Wang Renxin <hellotony521@qq.com>
2016-08-05 09:51:41 +0800

    *improved clean batch.

--- clean.cmd
+++ clean.cmd
@@ -14,11 +14,14 @@ del /f /s /q *.pdb
 del /f /s /q *.sdf
 del /f /s /q *.suo
 del /f /s /q *.user
+del /f /s /q *.vc.db
 
 del /f /s /q output\*.dep
 del /f /s /q output\*.htm
 del /f /s /q output\*.idb
 del /f /s /q output\*.ilk
+del /f /s /q output\*.iobj
+del /f /s /q output\*.ipdb
 del /f /s /q output\*.manifest
 del /f /s /q output\*.res
 del /f /s /q output\my_basic_d.exe
